9dad6bf550de9e283ad327ef76a453066e765dd0
Crew Spence <csspence123@gmail.com>
2021-02-21 14:12:52 -0600

    Wrote working solution.

--- codewars_true_for_any.js
+++ codewars_true_for_any.js
@@ -2,6 +2,11 @@
 Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
 */
 
-function any(arr, fun){
-  // ...
+const any = (arr, fun) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(fun(arr[i]) === true) {
+      return true;
+    }
+  }
+  return false;
 }
